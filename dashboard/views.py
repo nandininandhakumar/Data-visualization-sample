d65f9f2ce06afa6a3ff3424525677cf6a315a8c4
Nandini Nandhakumar <mailtonandininandus@gmail.com>
2022-05-10 15:30:37 +0530

    sorted and filtered

--- dashboard/views.py
+++ dashboard/views.py
@@ -21,30 +21,17 @@ def population_chart(request):
         'data': data,
     })
 
+
 def datatable_static(request):
     labels = []
     data = []
-    queryset = City.objects.values('country__name').annotate(country_population=Sum('population')).order_by('-country_population')
-    print(queryset[0].population)
-    # for entry in queryset:
-    #     labels.append(entry['country__name'])
-    #     data.append(entry['country_population'])
-    # data1 = {}
-    # data1['data'] = data
-    # data1['labels'] = labels
-    # data1 = dict(zip(labels,data))
-    # print("COUNTRY, POPULATION")
-    # for key, value in data1.items():
-    #     labels , data = value
-    #     print(labels, data)
-
     
-    # for key,values in data1.items():
-    #     for v in values:
-    #         print(key, ":" , v)
-
-    # context = {'out': data1.items()}
-    # print(context)
-    return render(request, 'datatable/datatable_static.html')
-    # return render (request,"datatable/datatable_static.html",data1)
+    queryset = City.objects.values('country__name').annotate(country_population=Sum('population')).order_by('-country_population')
+    
+    for entry in queryset:
+        labels.append(entry['country__name'])
+        data.append(entry['country_population'])
+ 
+    datas = zip(labels,data)
+    return render(request, 'datatable/datatable_static.html', {'data': datas})
         
